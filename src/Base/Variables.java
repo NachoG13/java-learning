d3f76a90e91a78999feb8e28e9c4e9b4eff8d89a
NachoG13 <ignacio.rover@gmail.com>
2024-07-07 20:14:04 -0300

    Commit

---
+++ src/Base/Variables.java
@@ -0,0 +1,46 @@
+package Base;
+
+public class Variables{
+    public static void main(String[] args) {
+    
+
+
+//Tipos Primitivos
+    //Tipos Numéricos Enteros:
+    byte myByte = -128; //8 bits, rango de -128 a 127.
+    short myShort = 32767; //16 bits, rango de -32,768 a 32,767.
+    int myInt = 1; //32 bits, rango de -2^31 a 2^31 - 1.
+    long myLong = 1; //64 bits, rango de -2^63 a 2^63 - 1
+    //Tipos Numéricos de Punto flotante
+    
+    float myFloat = 1; //32 bits, rango de aproximadamente ±3.40282347E+38F, sufijo f
+    double myDouble = 1.1; //64 bits, rango de aproximadamente ±1.79769313486231570E+308, sufijo d (o se puede omitir).
+    
+    //Tipo carácter
+    char myChar = 1; //16 bits Unicode, rango de '\u0000' (0) a '\uffff' (65,535)
+
+    //Tipo Booleano
+    boolean myBoolean1 = true;
+    boolean myBoolean2 = false;
+    
+//Tipo de Referencia (Objetos)
+    //Clases y Tipos definidos por el usuario
+    ///String
+    String myString = "Mi cadena de texto";
+    myString = "cambio el valor de la cadena";
+    System.out.println(myString);
+    ///Integer
+    
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,14 @@
-package src;
 
+// comentario 1
+/*
+Comentario 2
+*/
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hola mundo");
+        ///Variables
         
+
     }
 }
