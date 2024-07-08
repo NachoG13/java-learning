129c2c8a822ae9fb6400bb3ce14c2b3b28fa7136
NachoG13 <ignacio.rover@gmail.com>
2024-07-08 19:53:58 -0300

    Commit

--- readme.md
+++ readme.md
@@ -1,6 +1,24 @@
-## Hola
+# Hola
 
 Aquí se irán subiendo los avances y cositas sobre Java que voy teniendo
 
 ## Enlaces a los cursos utilizados
 - `Mouredev` : https://www.youtube.com/watch?v=W86KTBSiX2o&ab_channel=MoureDevbyBraisMoure
+
+# Cosas aprendidas
+
+## Vocabulario aprendido
+- parsear -> analizar una cadena de texto para analizar su estructura y sintáctica y extraer información sobre ella. 
+- InputStream -> 
+### Ingreso variable por consola
+Para ingresar datos por consola se necesita la clase Scanner -> Scanner entrada = new Scanner(System.in);
+- `Scanner` -> proporciona métodos convenientes para leer y parsear diferentes tipos de datos de un flujo de entrada.
+- `entrada` -> variable tipo Scanner
+- `System.in` -> Flujo de entrada estándar (generalmente el teclado)
+    - `System.out`: Flujo de salida estándar (generalmente la consola).
+    - `System.err`: Flujo de salida de error estándar (generalmente la consola, pero separado de System.out).
+
+### Conversión de datos
+
+No se puede almacenar una variable de un tipo de dato menor al de una suma de tipos de datos mayores.
+byte myByte =  myInt1 +  myInt2;
--- src/Base/Variables.java
+++ src/Base/Variables.java
@@ -1,5 +1,7 @@
 package Base;
 
+import java.util.Scanner;
+
 public class Variables{
     public static void main(String[] args) {
     
@@ -11,18 +13,23 @@ public class Variables{
     short myShort = 32767; //16 bits, rango de -32,768 a 32,767.
     int myInt = 1; //32 bits, rango de -2^31 a 2^31 - 1.
     long myLong = 1; //64 bits, rango de -2^63 a 2^63 - 1
+    
+    myByte = 9;
+
+
     //Tipos Numéricos de Punto flotante
     
     float myFloat = 1; //32 bits, rango de aproximadamente ±3.40282347E+38F, sufijo f
     double myDouble = 1.1; //64 bits, rango de aproximadamente ±1.79769313486231570E+308, sufijo d (o se puede omitir).
     
+
     //Tipo carácter
-    char myChar = 1; //16 bits Unicode, rango de '\u0000' (0) a '\uffff' (65,535)
+    char myChar = 'A'; //16 bits Unicode, rango de '\u0000' (0) a '\uffff' (65,535)
 
     //Tipo Booleano
     boolean myBoolean1 = true;
     boolean myBoolean2 = false;
-    
+
 //Tipo de Referencia (Objetos)
     //Clases y Tipos definidos por el usuario
     ///String
@@ -33,9 +40,25 @@ public class Variables{
     Integer myInteger = 10;
     System.out.println(myInteger);
 
+//CONVERSIÓN ENTRE VARIABLES
+///Ingresar una variable por consola
+Scanner entrada  = new Scanner(System.in);//System.in es un flujo de entrada estándar que lee datos desde la consola.
+
+System.out.println("Ingrese el primer número entero");
+int intUser = entrada.nextInt(); //nextInt te permite leer por consola un entero y lo almacena en IntUser
+entrada.nextLine();
+System.out.println("Ingrese el Segundo número entero");
+int intUser2 = entrada.nextInt();
+
+////No me deja convertir de un tipo de dato entero numérico grande a uno más chico
+// short resultado = intUser + intUser2;
+
+System.out.println("el resultado es "+ resultado);
+
+// Convertir tipos de datos
 
 
-
+entrada.close(); //tengo que cerrar el objeto tipo Scanner
 
 
 
